1ee73e53e0e7c41f710f6aed08612e7795751fb2
Claireyu <claireyu0328@gmail.com>
2013-11-22 16:22:04 +0900

    Add function of dynamically change number of layers.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
 $(function() {
+
 	// initialize touch spin
 	$('#layer-num').TouchSpin({
-		min: 0,
-		max: 100,
+		min: 1,
+		max: 8,
 		boostat: 5,
 		stepinterval: 200,
 		booster: false
 	});
+	
+	// on change
+	$('#layer-num').on( 'change', function() {
+		var count = $('.layer').length;
+		var num = $('#layer-num').val();
 
+		console.log('count:'+ count );
+		console.log('num:' + num );
+
+		// add layers
+		if ( num > count ) {
+			for ( var i = count + 1; i <= num; i++ ) {
+				$('.layer:last').after( 
+				'<div class="layer form-group">' +
+					'<label for="layer' + i + '" class="col-md-2 control-label">Layer' + i + '</label>' +
+					'<div class="col-md-4">' +
+						'<textarea id="layer' + i + '" class="form-control" rows="4"></textarea>' +
+					'</div>' +
+				'</div>');
+			}
+		}
+
+		// delete layers
+		else if ( num < count ) {
+			for ( var i = num; i <= count - 1; i++ ) {
+				$('.layer:last').remove();
+			}
+		}
+
+	});
 });
