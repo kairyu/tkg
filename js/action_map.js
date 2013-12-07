1d1402139053aaebdf10bae990fc99bb29c95777
Kai Ryu <kai1103@gmail.com>
2013-12-07 15:35:51 +0900

    Add group and name into action map

--- js/action_map.js
+++ js/action_map.js
@@ -1,13 +1,18 @@
 var action_map = {
 	"ACTION_NO": {
+		"group": "none",
 		"description": "",
 		"code": "0x0000"
 	},
+	/*
 	"ACTION_TRANSPARENT": {
 		"description": "",
 		"code": "0x0001"
 	},
+	*/
 	"ACTION_KEY": {
+		"group": "Kay action",
+		"name": "Normal key",
 		"description": "Send key",
 		"code": function(key) {
 			return "0x00" + dechex(key, 2);
@@ -15,6 +20,8 @@ var action_map = {
 		"param": [ "key" ]
 	},
 	"ACTION_MODS": {
+		"group": "Kay action",
+		"name": "Modifier",
 		"description": "Send modifier",
 		"code": function(mods, lr) {
 			if (lr) {
@@ -27,6 +34,8 @@ var action_map = {
 		"param": [ "mods", "lr" ]
 	},
 	"ACTION_MODS_KEY": {
+		"group": "Kay action",
+		"name": "Modified key",
 		"description": "Send modifier and key",
 		"code": function(key, mods, lr) {
 			if (lr) {
@@ -39,6 +48,8 @@ var action_map = {
 		"param": [ "key", "mods", "lr" ]
 	},
 	"ACTION_DEFAULT_LAYER_SET": {
+		"group": "Layer action",
+		"name": "Set default",
 		"description": "Set default layer",
 		"code": function(layer) {
 			if (validLayer(layer)) {
@@ -51,6 +62,8 @@ var action_map = {
 		"param": [ "layer" ]
 	},
 	"ACTION_LAYER_CLEAR": {
+		"group": "Layer advanced action",
+		"name": "Clear all",
 		"description": "Clear state of all layers",
 		"code": function(on) {
 			if (validOn(on)) {
@@ -60,6 +73,8 @@ var action_map = {
 		"param": [ "on" ]
 	},
 	"ACTION_LAYER_MOMENTARY": {
+		"group": "Layer action",
+		"name": "Momentary",
 		"description": "Turn layer momentarily on",
 		"code": function(layer) {
 			if (layer >= 0 && layer < 16) {
@@ -76,6 +91,8 @@ var action_map = {
 		"param": [ "layer" ]
 	},
 	"ACTION_LAYER_TOGGLE": {
+		"group": "Layer action",
+		"name": "Toggle",
 		"description": "Turn layer on/off",
 		"code": function(layer) {
 			if (validLayer(layer)) {
@@ -88,6 +105,8 @@ var action_map = {
 		"param": [ "layer" ]
 	},
 	"ACTION_LAYER_INVERT": {
+		"group": "Layer advanced action",
+		"name": "Invert",
 		"description": "Invert current state of layer",
 		"code": function(layer, on) {
 			if (validLayer(layer) && validOn(on)) {
@@ -100,6 +119,8 @@ var action_map = {
 		"param": [ "layer", "on" ]
 	},
 	"ACTION_LAYER_ON": {
+		"group": "Layer action",
+		"name": "On",
 		"description": "Turn layer on",
 		"code": function(layer, on) {
 			if (validLayer(layer) && validOn(on)) {
@@ -112,6 +133,8 @@ var action_map = {
 		"param": [ "layer", "on" ]
 	},
 	"ACTION_LAYER_OFF": {
+		"group": "Layer action",
+		"name": "Off",
 		"description": "Turn layer off",
 		"code": function(layer, on) {
 			if (validLayer(layer) && validOn(on)) {
@@ -124,6 +147,8 @@ var action_map = {
 		"param": [ "layer", "on" ]
 	},
 	"ACTION_LAYER_SET": {
+		"group": "Layer action",
+		"name": "Set",
 		"description": "Turn on layer only",
 		"code": function (layer) {
 			if (validLayer(layer)) {
@@ -136,6 +161,8 @@ var action_map = {
 		"param": [ "layer" ]
 	},
 	"ACTION_LAYER_ON_OFF": {
+		"group": "Layer advanced action",
+		"name": "On then off",
 		"description": "Turn on layer on press and turn off on release",
 		"code": function (layer) {
 			if (layer >= 0 && layer < 16) {
@@ -152,6 +179,8 @@ var action_map = {
 		"param": [ "layer" ]
 	},
 	"ACTION_LAYER_OFF_ON": {
+		"group": "Layer advanced action",
+		"name": "Off then on",
 		"description": "Turn off layer on press and turn on on release",
 		"code": function (layer) {
 			if (layer >= 0 && layer < 16) {
@@ -168,6 +197,8 @@ var action_map = {
 		"param": [ "layer" ]
 	},
 	"ACTION_LAYER_SET_CLEAR": {
+		"group": "Layer advanced action",
+		"name": "Set then clear",
 		"description": "Set layer state on press and clear on release",
 		"code": function (layer) {
 			if (layer >= 0 && layer < 16) {
@@ -184,18 +215,26 @@ var action_map = {
 		"param": [ "layer" ]
 	},
 	"ACTION_BACKLIGHT_INCREASE": {
+		"group": "Backlight action",
+		"name": "Increase",
 		"description": "Increase backlight brightness",
 		"code": "0xD000"
 	},
 	"ACTION_BACKLIGHT_DECREASE": {
+		"group": "Backlight action",
+		"name": "Decrease",
 		"description": "Decrease backlight brightness",
 		"code": "0xD001"
 	},
 	"ACTION_BACKLIGHT_TOGGLE": {
+		"group": "Backlight action",
+		"name": "Toggle",
 		"description": "Turn backlight on/off",
 		"code": "0xD002"
 	},
 	"ACTION_BACKLIGHT_STEP": {
+		"group": "Backlight action",
+		"name": "Step",
 		"description": "Step through backlight levels",
 		"code": "0xD003"
 	}
